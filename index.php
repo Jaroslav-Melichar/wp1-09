7d3e79995d8a47750c4159bc078d68e8dd3f2eaa
Jaroslav-Melichar <jarabaltazar@seznam.cz>
2021-06-01 18:23:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,12 @@
     <title>desing</title>
 </head>
 <body>
-<h1>Konfigurátor Mercedes-Benz</h1>
+<h1>Konfigurátor značky Mercedes-Benz</h1>
 
-<label for="cars">Vyberte model auta</label>
-
+<label for="cars">Vyberte model auta</label> 
+<br>
 <select name="cars" id="cars">
+<option value="other"></option>
   <option value="model1">GT-63 AMG</option>
   <option value="model2">GT-63 BRABUS</option>
   <option value="model3">C-63 AMG</option>
@@ -20,17 +21,42 @@
   <option value="model6">E-63 AMG</option>
 </select>
 <br>
-<input type="checkbox" id="Benzin" name="Benzin" value="Benzin">
-<label for="Benzin"> Benzín</label><br>
-<input type="checkbox" id="Diesel" name="Diesel" value="Diesel">
-<label for="Diesel"> Diesel</label><br>
-<input type="checkbox" id="LPG" name="LPG" value="LPG">
-<label for="LPG"> LPG</label><br>
-<input type="checkbox" id="Elektro" name="Elektro" value="Elektro">
+<h3>Vyberte jaké chcete palivo do vozidla</h3>
+<input type="radio" id="fuel5" name="Fuel" value="Natural">
+<label for="Natural">Natural</label><br>
+<input type="radio" id="fuel4" name="Fuel" value="Diesel">
+<label for="Diesel">Diesel</label><br>
+<input type="radio" id="fuel3" name="Fuel" value="LPG">
+<label for="LPG">LPG</label><br>
+<input type="radio" id="fuel2" name="Fuel" value="Elektro">
 <label for="Elektro">Elektro</label><br>
-<input type="checkbox" id="Hybrid" name="Hybrid" value="Hybrid">
-<label for="Hybrid"> Hybrid</label><br>
-
+<input type="radio" id="fuel1" name="Fuel" value="Hybrid">
+<label for="Hybrid">Hybrid</label><br>
+<br>
+<h3>Výbavy vozidla které si můžete přikoupit</h3>
+<input type="checkbox" id="vehicle1" name="vehicle1" value="tuning1">
+<label for="vehicle1">Zatmavené skla</label><br>
+<input type="checkbox" id="vehicle2" name="vehicle2" value="tuning2">
+<label for="vehicle2">Vyhřívané čelní sklo</label><br>
+<input type="checkbox" id="vehicle3" name="vehicle3" value="tuning3">
+<label for="vehicle3">Premiová klimatizace</label><br>
+<input type="checkbox" id="vehicle1" name="vehicle1" value="tuning4">
+<label for="vehicle1">Prémiové Reproduktory se 7.1 zvukem po vozidle</label><br>
+<input type="checkbox" id="vehicle2" name="vehicle2" value="tuning5">
+<label for="vehicle2">Kožené sedačky</label><br>
+<input type="checkbox" id="vehicle3" name="vehicle3" value="tuning6">
+<label for="vehicle3">Červené koženné sedačky</label><br>
+<br>
+<label for="cars"> <h3>Vyberte barvu vašeho vozidla</h3> </label> 
+<select name="colors" id="colors">
+  <option value="other1"></option>
+  <option value="color1">Tmavě modrá</option>
+  <option value="color2">Černá</option>
+  <option value="color3">Šedivá</option>
+  <option value="color4">Červená</option>
+  <option value="color5">Růžová</option>
+  <option value="color6">Žlutá</option>
+</select>
 
     
 </body>
