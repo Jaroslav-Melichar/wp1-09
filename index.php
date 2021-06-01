416b8f8fbe22dee660c2eb08df112e151b0e527d
Jaroslav-Melichar <jarabaltazar@seznam.cz>
2021-06-01 12:30:04 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>desing</title>
+</head>
+<body>
+<h1>Konfigurátor Mercedes-Benz</h1>
+
+<label for="cars">Vyberte model auta</label>
+
+<select name="cars" id="cars">
+  <option value="model1">GT-63 AMG</option>
+  <option value="model2">GT-63 BRABUS</option>
+  <option value="model3">C-63 AMG</option>
+  <option value="model4">GT-S</option>
+  <option value="model5">GT-R</option>
+  <option value="model6">E-63 AMG</option>
+</select>
+    
+</body>
+</html>
